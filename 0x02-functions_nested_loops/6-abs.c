a8785511cd4f2d0883a1a18d9663fed26aaa0965
Amaka Erugo <chiamaka.erugo@stu.cu.edu.ng>
2023-04-25 15:32:18 -0700

    sdf

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - absolute of integer passed
+ *
+ * @n: integer
+ * Return: absolute of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+
+	return (n);
+}
