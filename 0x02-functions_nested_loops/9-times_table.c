fed41622f680e0a765db08b212e6ac9c154c2763
Amaka Erugo <chiamaka.erugo@stu.cu.edu.ng>
2023-04-25 16:53:51 -0700

    dsa

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+	 		if ((i * j) <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + (i * j));
+			}
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0' + ((i * j) / 10));
+			_putchar('0' + ((i * j) % 10));	
+		}
+		_putchar('\n');
+	}
+}
