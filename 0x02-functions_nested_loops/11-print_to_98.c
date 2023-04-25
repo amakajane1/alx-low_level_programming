823c5b9e32ed9edc4c69586bd0df610bfc8e3dd0
Amaka Erugo <chiamaka.erugo@stu.cu.edu.ng>
2023-04-25 16:10:10 -0700

    sp

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers from n to 98
+ *
+ * @n: number to start from
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+				printf("%i, ", n);
+			else
+				printf("%i\n", n);
+		}
+	} else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+				printf("%i, ", n);
+			else
+				printf("%i\n", n);
+		}
+	}
+}
