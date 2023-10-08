d4c1889b92ff9c36347cc8c95901645c5da988f8
amakajane1 <chiamaka.erugo@stu.cu.edu.ng>
2023-10-07 22:10:59 -0700

    ltrty

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // Get the program name from argv[0]
+    char *program_name = argv[0];
+
+    // Print the program name followed by a new line
+    printf("%s\n", program_name);
+
+    return 0;
+}
+
